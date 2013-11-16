eebd5289c1c8743ca9c675a0150bfaae6eab7146
coolbit <coolbit.in@gmail.com>
2013-11-16 17:39:32 +0800

    增加django框架文件

---
+++ linux_xidian/apps/mirrors_status/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
+
+def show_mirrors_status(request):
+    pass
